148b32dbf0a9e9867fae1fcc7d3fe030f5889508
Jeff White <jeffrey.white@lexisnexisrisk.com>
2024-07-23 16:48:00 -0700

    0.0.1 version

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FlyreelFNOL",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "FlyreelFNOL",
+            targets: ["FlyreelFNOL"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "FlyreelFNOL",
+            path: "FlyreelFNOL.xcframework"
+        )
+    ]
+)
